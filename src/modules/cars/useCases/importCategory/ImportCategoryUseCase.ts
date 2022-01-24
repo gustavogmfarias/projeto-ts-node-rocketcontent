e69da21df46fa5c4a01f71f55c6ee3e2ab9670c9
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-01-24 09:46:52 -0300

    Erro swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -27,9 +27,19 @@ class ImportCategoryUseCase {
         .on('error', err => reject(err));
     });
   }
+
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories);
+
+    categories.map(async category => {
+      const { name, description } = category;
+      const existCategory = this.categoriesRepository.findByName(name);
+      if (!existCategory) {
+        this.categoriesRepository.create({ name, description });
+      }
+
+      return;
+    });
   }
 }
 
