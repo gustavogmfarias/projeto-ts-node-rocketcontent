60e04d15a848a53a4b6c3f790d035d55a5adefd8
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-02-11 16:47:10 -0300

    docker doesn't work

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -14,7 +14,7 @@ class ImportCategoryUseCase {
     return new Promise((resolve, reject) => {
       const categories: IImportCategory[] = [];
       const stream = fs.createReadStream(file.path);
-      const parseFile = parse();
+      const parseFile = parse(); //diz que vamos usar csv separado por virgula
       stream.pipe(parseFile);
       parseFile
         .on('data', async line => {
@@ -22,6 +22,7 @@ class ImportCategoryUseCase {
           categories.push({ name, description });
         })
         .on('end', () => {
+          fs.promises.unlink(file.path); //remove o arquivo que subimos;
           resolve(categories);
         })
         .on('error', err => reject(err));
