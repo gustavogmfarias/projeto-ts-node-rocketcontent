ac27b2a27f1ebf4f3305c34e7f5e7e540bfedbc0
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-02-17 17:13:11 -0300

    verificar bd

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 import { router } from './routes';
-import './databese';
+import './database';
 const app = express();
 
 app.use(express.json());
