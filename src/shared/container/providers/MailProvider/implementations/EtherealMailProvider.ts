1ef3b925370cc759676d4cb40e79d0950cd5024a
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-04-27 20:32:52 -0300

    .

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,6 +1,8 @@
 import { injectable } from 'tsyringe';
 import nodemailer, { Transporter } from 'nodemailer';
 import { IMailProvider } from '../IMailProvider';
+import handlebars from 'handlebars';
+import fs from 'fs';
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
@@ -9,7 +11,7 @@ class EtherealMailProvider implements IMailProvider {
     nodemailer
       .createTestAccount()
       .then(account => {
-        const trasporter = nodemailer.createTransport({
+        const transporter = nodemailer.createTransport({
           host: account.smtp.host,
           port: account.smtp.port,
           secure: account.smtp.secure,
@@ -19,18 +21,26 @@ class EtherealMailProvider implements IMailProvider {
           },
         });
 
-        this.client = trasporter;
+        this.client = transporter;
       })
       .catch(err => console.error(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string,
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+    const templateParse = handlebars.compile(templateFileContent);
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: 'Rentx <noreply@rentex.com.br>',
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log('Message sent: %s', message.messageId);
