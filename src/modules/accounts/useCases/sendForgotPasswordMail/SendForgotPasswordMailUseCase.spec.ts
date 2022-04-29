0725639943db84f048beb82c50cb0044ab6276fa
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-04-29 11:04:44 -0300

    .

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -1,3 +1,39 @@
+import { UsersRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersRepositoryInMemory';
+import { UsersTokensRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory';
+import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
+import { MailProviderInMemory } from '@shared/container/providers/MailProvider/in-memory/MailProviderInMemory';
+import { SendForgotPasswordMailUseCase } from './SendForgotPasswordMailUseCase';
+
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let dateProvider: DayjsDateProvider;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let mailProvider: MailProviderInMemory;
+
 describe('Send forgot e-mail', () => {
-  it('should be able to send a forgot password e-mail to user', () => {});
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    dateProvider = new DayjsDateProvider();
+    usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    mailProvider = new MailProviderInMemory();
+
+    sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+      usersRepositoryInMemory,
+      usersTokensRepositoryInMemory,
+      dateProvider,
+      mailProvider,
+    );
+  });
+  it('should be able to send a forgot password e-mail to user', async () => {
+    const sendMail = jest.spyOn(mailProvider, 'sendMail');
+
+    await usersRepositoryInMemory.create({
+      driver_license: '443901',
+      email: 'laiji@giwafgo.mk',
+      name: 'Mae Zimmerman',
+      password: '1234',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
 });
--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -6,25 +6,29 @@ import fs from 'fs';
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
-  private client: Transporter;
   constructor() {
-    nodemailer
-      .createTestAccount()
-      .then(account => {
-        const transporter = nodemailer.createTransport({
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-            user: account.user,
-            pass: account.pass,
-          },
-        });
-
-        this.client = transporter;
-      })
-      .catch(err => console.error(err));
+    this.createClient();
   }
+
+  private async createClient() {
+    try {
+      const account = await nodemailer.createTestAccount();
+
+      this.client = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+    } catch (err) {
+      console.error(`EtherealMailProvider - Error:\n${err}`);
+    }
+  }
+
+  private client: Transporter;
 
   async sendMail(
     to: string,
@@ -32,6 +36,10 @@ class EtherealMailProvider implements IMailProvider {
     variables: any,
     path: string,
   ): Promise<void> {
+    if (!this.client) {
+      await this.createClient();
+    }
+
     const templateFileContent = fs.readFileSync(path).toString('utf-8');
     const templateParse = handlebars.compile(templateFileContent);
     const templateHTML = templateParse(variables);
