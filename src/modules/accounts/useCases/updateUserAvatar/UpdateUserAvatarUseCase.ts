082611804463138cfbd1fe066d3641fad6688f05
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-05-03 14:18:09 -0300

    .

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -20,8 +20,10 @@ class UpdateUserAvatarUseCase {
     const user = await this.usersRepository.findById(user_id);
 
     if (user.avatar) {
-      await this.storageProvider.save(avatar_file, 'avatar');
+      await this.storageProvider.delete(user.avatar, 'avatar');
     }
+
+    await this.storageProvider.save(avatar_file, 'avatar');
 
     user.avatar = avatar_file;
 
